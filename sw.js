b9759f091b2bcf337d2342fc564806ea6bfd8be7
lenoloc <36268603+lenoloc@users.noreply.github.com>
2026-07-17 14:46:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,1 @@
-const CACHE_NAME = 'lastwar-v5-2-cache-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
-});
+self.addEventListener('install', function(){ self.skipWaiting(); }); self.addEventListener('activate', function(event){ event.waitUntil( caches.keys().then(function(names){ return Promise.all(names.map(function(n){ return caches.delete(n); })); }).then(function(){ return self.clients.matchAll(); }).then(function(clients){ clients.forEach(function(c){ c.navigate(c.url); }); return self.registration.unregister(); }) ); });
