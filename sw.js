07da486c97a1c976384c9abc46bd22ed73ecaae8
lenoloc <36268603+lenoloc@users.noreply.github.com>
2026-07-17 14:45:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,1 @@
-self.addEventListener('install', ** => {
-    self.skipWaiting();
-});**self.addEventListener('fetch', ()**> {});
+self.addEventListener('install', function(){ self.skipWaiting(); }); self.addEventListener('activate', function(event){ event.waitUntil( caches.keys().then(function(names){ return Promise.all(names.map(function(n){ return caches.delete(n); })); }).then(function(){ return self.clients.matchAll(); }).then(function(clients){ clients.forEach(function(c){ c.navigate(c.url); }); return self.registration.unregister(); }) ); });
